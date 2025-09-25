6600ea814973009fc50ead3e6c22261ff43a1788
Saniyat Al Ahmed <saniyat013@gmail.com>
2025-09-25 12:11:28 +0600

    Added Global Exception Handler for Invalid Arguments

--- patient-service/src/main/java/com/saniyat/patientservice/dto/PatientRequestDTO.java
+++ patient-service/src/main/java/com/saniyat/patientservice/dto/PatientRequestDTO.java
@@ -6,11 +6,11 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class PatientRequestDTO {
-    @NotBlank
+    @NotBlank(message = "Name is required")
     @Size(max = 100, message = "Name cannot exceed 100 characters")
     private String name;
 
-    @NotBlank
+    @NotBlank(message = "Email is required")
     @Email(message = "Email should be valid")
     private String email;
 
